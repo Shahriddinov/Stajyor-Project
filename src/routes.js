96f256c8c4d2758e50b7f1c3241f981dfffc2bf1
Fazliddin-Nasrullayev <fazliddinnasrullayev2004@gmail.com>
2023-01-20 11:51:01 +0500

    feat: login page

--- src/routes.js
+++ src/routes.js
@@ -3,32 +3,52 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { history } from "store";
 import { Spinner, Layout } from "components";
 import { getBaseName } from "./utils";
-
 import App from "./App";
 import ScrollTop from "./hoc/ScrollTop";
 
+
 const Home = lazy(() => import("./pages/Home"));
 const NotFound = lazy(() => import("./pages/404"));
+const Login = lazy(() => import('./pages/Sign/Login/Login'))
+const Signup = lazy(() => import('./pages/Sign/Signup/Signup'))
 
 const routes = [
-	{ path: "", element: Home },
+	{ path: "", element: Home }
 
 ];
 
+
+const auth_path = window.location.pathname.split("/")[2]
+
 const RoutesContainer = () => (
+
+
 	<Router {...{ history }} basename={`/${getBaseName()}`}>
 		<App>
-			<Layout>
-				<Suspense fallback={<Spinner position="full" />}>
+				{
+					auth_path === 'login' ? 
+			<Routes>
+				<Route path="login" element={ <Login /> } />
+			</Routes> :
+			auth_path === 'signup' ?
+			<Routes>
+				<Route path="signup" element={ <Signup /> } />
+			</Routes> :
+			
+
+
+					 <Layout>
+						<Suspense fallback={<Spinner position="full" />}>
 					<Routes>
 						{routes.map((route, key) => {
 							const RouteComponent = ScrollTop(route.element);
 							return <Route key={key} path={route.path} element={<RouteComponent />} />;
-						})}
+						})} 
 						<Route path="*" element={<NotFound />} />
-					</Routes>
-				</Suspense>
-			</Layout>
+						</Routes>
+						</Suspense>
+					</Layout> 
+				}
 		</App>
 	</Router>
 );

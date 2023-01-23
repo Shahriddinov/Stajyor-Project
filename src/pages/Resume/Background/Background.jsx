a6ce190915a2705413fe135def45045cfecb2536
Qayumov Pahlavon <qayumovpahlavon2778tatu@gmail.com>
2023-01-23 12:01:18 +0500

    Add mycareer components

--- src/pages/Resume/Background/Background.jsx
+++ src/pages/Resume/Background/Background.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./Background.module.scss";
 import back from "../../../assets/images/Resume/back.png";
+import MyCareer from "../MyCareer/MyCareer";
 
 function Background() {
 	return (
@@ -13,8 +14,13 @@ function Background() {
 					Back
 				</Link>
 			</div>
-			<div className={classes.round}>
+
+			{/* <div className={classes.round}>
 				<Round />
+			</div> */}
+
+			<div className={classes.mycareer}>
+				<MyCareer />
 			</div>
 		</div>
 	);

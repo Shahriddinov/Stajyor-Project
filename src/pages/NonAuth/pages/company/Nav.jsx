ebaa31ca11b4ce0173992c169a1e1d1b8e30a95f
Jasur <76224872+JasurNurgiljonov@users.noreply.github.com>
2023-02-16 20:02:26 +0500

    JobAd page added

---
+++ src/pages/NonAuth/pages/company/Nav.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import classes from './Company.module.scss'
+import classesNav from './Navigation.module.scss'
+import { Link } from 'react-router-dom'
+import Logo from './assets/logo.png'
+
+const Nav = () => {
+
+    const [count, setCount] = useState(0);
+    const [clientJobs, setClientJobs] = useState('active')
+
+	let step1 = false,
+		step2 = false,
+		step3 = false,
+		step4 = false,
+		step5 = false,
+		step6 = false,
+		step7 = false,
+		step8 = false;
+	switch (count) {
+		case 1:
+			step1 = true;
+			break;
+		case 2:
+			step2 = true;
+			break;
+		case 3:
+			step3 = true;
+			break;
+		case 4:
+			step4 = true;
+			break;
+		case 5:
+			step5 = true;
+			break;
+		case 6:
+			step6 = true;
+			break;
+
+		case 7:
+			step7 = true;
+			break;
+
+		case 8:
+			step8 = true;
+			break;
+
+		default:
+			step1 = true;
+	}
+
+    return (
+        <div className={classes.background}>
+            <div className={classesNav.menu}>
+                <a href=''><img src={Logo} alt="" /></a>
+                <div className={classesNav.right__side}>
+                    <ul className={classesNav.menu__links}>
+                        <li className={step1 ? classesNav.active : classesNav.menu__link} onClick={() => setCount(1)}>
+                            Home
+                        </li>
+                        <li className={step5 ? classesNav.active : classesNav.menu__link} onClick={() => setCount(5)}>
+                            Talants
+                        </li>
+                        <li className={step6 ? classesNav.active : classesNav.menu__link} onClick={() => setCount(6)}>
+                            Jobs
+                        </li>
+                        <li className={step7 ? classesNav.active : classesNav.menu__link} onClick={() => setCount(7)}>
+                            About us
+                        </li>
+                        <li className={step8 ? classesNav.active : classesNav.menu__link} onClick={() => setCount(8)}>
+                            Contact us
+                        </li>
+                    </ul>
+                    <div className={classesNav.menu__buttons}>
+                        <Link to="/login">
+                            <button className={classesNav.menu__login}>Log in</button>
+                        </Link>
+                        <Link to="/signup">
+                            <button className={classesNav.menu__signup}>Sign up</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav

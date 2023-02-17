b1834fef29c83172c3d1d7c0b0e9a13dae6093b2
AlmazKhambarov <almazkhambarov07@gmail.com>
2023-02-17 20:10:24 +0500

    contact US Start

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import './Header.scss'
 
-<<<<<<< HEAD
-=======
-import header_logo from '../../../assets/images/header/logo.svg'
-import header_logo_bg from '../../../assets/images/header/ellipse.png'
->>>>>>> cd612be4ffdd9ad2f287915a88609d7dca1a6a47
 // import header_logo from '../../../assets/images/Freelancer/Freelancer_logo.svg'
 // import header_logo_bg from '../../../assets/images/Freelancer/frilancer_ellipse.png'
 import Dropdown from "./components/Dropdown";
@@ -25,13 +20,8 @@ const Header = () => {
 	return (
 		<header className="header" >
 			<div className="header_container">
-<<<<<<< HEAD
 				{/* <img src={header_logo} alt="" /> */}
 				{/* <img src={header_logo_bg} className='header_container_bg_img'  alt="header bg img" /> */}
-=======
-				<img src={header_logo} className='header_container_bg_logo' alt="" />
-				<img src={header_logo_bg} className='header_container_bg_img'  alt="header bg img" />
->>>>>>> cd612be4ffdd9ad2f287915a88609d7dca1a6a47
 
 				
 				<div className="header_wrapper">

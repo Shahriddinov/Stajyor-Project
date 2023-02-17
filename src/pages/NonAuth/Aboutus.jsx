816888cce0f61d5f69584177820656c3784b48a2
unknown <oakhmadjonov1@gmail.com>
2023-02-17 12:42:23 +0500

    For help

--- src/pages/NonAuth/Aboutus.jsx
+++ src/pages/NonAuth/Aboutus.jsx
@@ -8,13 +8,17 @@ function Aboutus(props) {
 	return (
 		<div className={classes.aboutus}>
 			<div className={classes.mainAbout}>
-				<video width="950" height="536" className={classes.Video} controls src="https://youtu.be/sUbR1uZ9G3A" type="video/mp4"></video>
+				<div className={classes.videoBlock}>
+					<video className={classes.Video} controls src="https://youtu.be/sUbR1uZ9G3A" type="video/mp4"></video>
+				</div>
 				<div className={classes.TextsMain}>
 					<div className={classes.IconsMain}>
 						<p>About us</p>
-						<img src={TelegramIcon} alt="Telegram Icon" />
-						<img src={FacebookIcon} alt="Facebook Icon" />
-						<img src={InstagramIcon} alt="Instagram Icon" />
+						<div className={classes.iconsBlock}>
+							<img src={TelegramIcon} alt="Telegram Icon" />
+							<img src={FacebookIcon} alt="Facebook Icon" />
+							<img src={InstagramIcon} alt="Instagram Icon" />
+						</div>
 					</div>
 					<p className={classes.text}>
 						Habitant mi varius consectetur eget. Neque, arcu, egestas pellentesque vitae vitae ut. Est malesuada consectetur quam netus sollicitudin
@@ -22,11 +26,11 @@ function Aboutus(props) {
 						<br />
 						<br />
 						At maecenas metus pellentesque turpis egestas mattis neque. Tempor amet tortor phasellus hac quisque. Molestie sollicitudin lorem
-						habitasse erat velit, donec elit habitant. Tortor pharetra, mattis ac mauris habitasse purus massa fusce mollis. Ullamcorper augue
-						feugiat feugiat urna feugiat.
+						habitasse erat velit, donec elit habitant. habitasse erat velit, donec elit habitant.
 						<br />
 						<br />
-						Eu hendrerit tellus at felis, mi, imperdiet. Porta risus odio tortor, etiam. In.
+						Eu hendrerit tellus at felis, mi, imperdiet. Porta risus odio tortor, etiam. Eu hendrerit tellus at felis, mi, imperdiet. Porta risus
+						odio tortor, etiam.
 					</p>
 				</div>
 			</div>
--- src/pages/NonAuth/Contactus.jsx
+++ src/pages/NonAuth/Contactus.jsx
@@ -5,27 +5,27 @@ import logosWhatsapp from "../../assets/images/icons/logos_whatsapp.png";
 
 function Contactus(props) {
 	return (
-		<>
+		<div className={classes.contactContainer}>
 			<div className={classes.mainContacts}>
-				<div className={classes.telegramContact}>
+				{/*<div className={classes.telegramContact}>
 					<img src={logosInstagram} alt="" />
 					<h1>Go To Telegram</h1>
 				</div>
 				<div className={classes.whatsappContact}>
 					<img src={logosWhatsapp} alt="" />
 					<h1>Go To Whatsapp</h1>
-				</div>
+	</div>*/}
 			</div>
 			<div className={classes.contactUs}>
-				<h1>Contact us</h1>
+				{/*<h1>Contact us</h1>
 				<p>Fill in the blank and we will contact you</p>
 				<input type="text" placeholder="Name" required />
 				<input type="email" placeholder="E-mail" required />
 				<input type="number" placeholder="+998 99 999 99 99" min="1" max="13" required />
 				<input className={classes.textMessage} type="text" placeholder="Text message" required />
-				<button>Send</button>
+	<button>Send</button>*/}
 			</div>
-		</>
+		</div>
 	);
 }
 

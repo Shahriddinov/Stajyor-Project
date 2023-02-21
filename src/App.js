59bde8e3c40ee99bea9fee2605f95baa97ac5e05
Jakhongir <jakhongiryusupov58@gmail.com>
2023-02-21 14:28:57 +0500

    bags cleared

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useSelector } from "react-redux";
-import { Route, Routes,Navigate,useNavigate} from "react-router-dom";
-import Home from "pages/Home";
+import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import Aboutus from "pages/NonAuth/Aboutus";
 import Talants from "pages/NonAuth/pages/talants/Talants";
 import Jobs from "pages/NonAuth/pages/jobs/Jobs";
@@ -14,7 +13,6 @@ import Background from "pages/Resume/Background/Background";
 import ResumeFinish from "pages/Resume/ResumeFinish";
 import Contract from "pages/contract";
 import Contactus from "pages/NonAuth/Contactus";
-import { useEffect } from "react";
 import Freelancer from "pages/Freelancer/Freelancer";
 import Profile from "pages/FreelancerProfile/Profile";
 import UserFreelancer from "pages/Freelancer/UserFreelancer";
@@ -27,51 +25,51 @@ function App() {
 
 	useLayoutEffect(() => {
 		navigate(`/${len}/`)
-	},[len])
+	}, [len])
 
 	const pathName = window.location.pathname;
 
-	return(
+	return (
 		<div className="App">
 			{
 				!auth
-				?(
-					<Routes>
-						<Route path={`/${len}/`} element={<PageBackground/>}/>
-						<Route path={`/${len}/login`} element={<Login/>}/>
-						<Route path={`/${len}/sign-up`} element={<Signup/>}/>
-						<Route path="*" element={<Navigate to={`/${len}/`}/>}/>
-					</Routes>
-				)
-				:
-				!resume
-				?(
-					<Routes>
-						<Route path={`/${len}/company`} element={<Login/>}/>
-						<Route path={`/${len}/resume`} element={<Background/>}/>
-						<Route path={`/${len}/resume-finish/:resumeId`} element={<ResumeFinish/>}/>
-						<Route path="*" element={<Navigate to={`/${len}/company`}/>}/>
-					</Routes>
-				)
-				:(
-					<>
-						<Header/>
+					? (
 						<Routes>
-							<Route path={`/${len}/jobs`} element={<Jobs/>}/>
-							<Route path= {`/${len}/about`} element={<Aboutus/>}/>
-							<Route path={`/${len}/talants`} element={<Talants/>}/>
-							<Route path={`/${len}/contact`} element={<Contactus/>}/>
-							<Route path={`/${len}/contracts`} element={<Contract/>}/>
-							<Route path={pathName.slice(0,4) } element={<Navigate to={`/${len}/jobs`}/>}/>
-							<Route path={`/${len}/resume-finish/:resumeId`} element={<Navigate to={`/${len}/jobs`}/>}/>
-							<Route path={`/${len}/freelancer`} element={<Freelancer/>}/>
-							<Route path={`/${len}/profil`} element={<Profile/>}/>
-							<Route path={`/${len}/freelancer-user`} element={<UserFreelancer/>}/>
-							<Route path={`/${len}/*`} element={<NotFound/>}/>
+							<Route path={`/${len}/`} element={<PageBackground />} />
+							<Route path={`/${len}/login`} element={<Login />} />
+							<Route path={`/${len}/sign-up`} element={<Signup />} />
+							<Route path="*" element={<Navigate to={`/${len}/`} />} />
 						</Routes>
-					</>
-				  )
-			 }
+					)
+					:
+					!resume
+						? (
+							<Routes>
+								<Route path={`/${len}/company`} element={<Login />} />
+								<Route path={`/${len}/resume`} element={<Background />} />
+								<Route path={`/${len}/resume-finish/:resumeId`} element={<ResumeFinish />} />
+								<Route path="*" element={<Navigate to={`/${len}/company`} />} />
+							</Routes>
+						)
+						: (
+							<>
+								<Header />
+								<Routes>
+									<Route path={`/${len}/jobs`} element={<Jobs />} />
+									<Route path={`/${len}/about`} element={<Aboutus />} />
+									<Route path={`/${len}/talants`} element={<Talants />} />
+									<Route path={`/${len}/contact`} element={<Contactus />} />
+									<Route path={`/${len}/contracts`} element={<Contract />} />
+									<Route path={pathName.slice(0, 4)} element={<Navigate to={`/${len}/jobs`} />} />
+									<Route path={`/${len}/resume-finish/:resumeId`} element={<Navigate to={`/${len}/jobs`} />} />
+									<Route path={`/${len}/freelancer`} element={<Freelancer />} />
+									<Route path={`/${len}/profil`} element={<Profile />} />
+									<Route path={`/${len}/freelancer-user`} element={<UserFreelancer />} />
+									<Route path={`/${len}/*`} element={<NotFound />} />
+								</Routes>
+							</>
+						)
+			}
 		</div>
 	)
 }
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -7,43 +7,44 @@ import Dropdown from "./components/Dropdown";
 import LangDrop from "./components/LangDrop";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
 	const len = useSelector(state => state.lenguage.lenguage)
-	const {t} = useTranslation()
+	const { t } = useTranslation()
 
 	return (
 		<header className="header" >
 			<div className="header_container">
-				 <img src={header_logo} className='header_container_bg_logo' alt="" />
-				<img src={header_logo_bg} className='header_container_bg_img'  alt="header bg img" />
+				<img src={header_logo} className='header_container_bg_logo' alt="" />
+				<img src={header_logo_bg} className='header_container_bg_img' alt="header bg img" />
 
-				
+
 				<div className="header_wrapper">
 
-				<ul className="header_container_list">
+					<ul className="header_container_list">
 
 
-					<li className="header_container_list_item">
-						{/* <a href={`${auth_path}/contact_us`} className="header_container_list_item">Contact us</a> */}
-						<Link to={`/${len}/jobs`} className="header_container_list_item">Jobs</Link>
-					</li>
-					
-					<li className="header_container_list_item">
-						<Link to={`/${len}/contact`} className="header_container_list_item">{t("contact")}</Link>
-					</li>
-					
-					<li className="header_container_list_item">
-						<Link to={`/${len}/about`} className="header_container_list_item">{t("about")}</Link>
-					</li>
-					<span className="header_container_list_span"></span>
-				</ul>
-						<Dropdown/>
-						<LangDrop/>
+						<li className="header_container_list_item">
+							{/* <a href={`${auth_path}/contact_us`} className="header_container_list_item">Contact us</a> */}
+							<Link to={`/${len}/jobs`} className="header_container_list_item">Jobs</Link>
+						</li>
+
+						<li className="header_container_list_item">
+							<Link to={`/${len}/contact`} className="header_container_list_item">{t("contact")}</Link>
+						</li>
+
+						<li className="header_container_list_item">
+							<Link to={`/${len}/about`} className="header_container_list_item">{t("about")}</Link>
+						</li>
+						<span className="header_container_list_span"></span>
+					</ul>
+					<Dropdown />
+					<LangDrop />
 
 				</div>
 
-			
+
 			</div>
 		</header>
 	);

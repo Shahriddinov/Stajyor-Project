6d5c14a28c7ec1de1a9ee6723e20740fbd94aa75
AlmazKhambarov <almazkhambarov07@gmail.com>
2023-05-04 11:16:04 +0500

    resume in process

--- src/pages/Resume/ResumeFinish/index.js
+++ src/pages/Resume/ResumeFinish/index.js
@@ -5,7 +5,7 @@ import arrowLeft from "../../../assets/images/arrow-left.svg";
 import logo from "../../../assets/images/Logo.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import { resumeSelect, resumeFinishPost } from "reduxToolkit/extraReducers";
+import { resumeSelect, resumeFinishPost, getFreelancer } from "reduxToolkit/extraReducers";
 import Resume1 from "./complate-resume/resume-list/Resume1";
 import Resume2 from "./complate-resume/resume-list/Resume2";
 import Resume3 from "./complate-resume/resume-list/Resume3";
@@ -17,10 +17,11 @@ import { activeDoteAction } from "reduxToolkit/resumeControlsSlice/resumeControl
 const ReumeFinish = () => {
 	// const resumeDetails = useSelector(state => state.resume.resumeDetails)
 	const { ...freelancer } = useSelector(state => state.frilanserCardSlice.freelancer);
-	console.log(freelancer);
+	const {freelancerLoading, } = useSelector(state=> state.resume)
+	console.log(freelancerLoading)
 	const loading = useSelector(state => state.resume.loading);
 	const experiences = useSelector(state => state.resume.experienceList);
-	const skillsData = useSelector(state => state.frilanserCardSlice.skillsData);
+	const {skillsData, freelancerData} = useSelector(state => state.frilanserCardSlice);
 	const hobbiesData = useSelector(state => state.frilanserCardSlice.hobbiesData);
 	const resumeOnSuccess = useSelector(state => state.login.resumeOnSuccess);
 	const dispatch = useDispatch();
@@ -37,7 +38,16 @@ const ReumeFinish = () => {
 		},
 		[resumeOnSuccess]
 	);
-
+useEffect(()=>{
+if (!freelancerLoading) {
+	let id = localStorage.getItem('freelancerId')
+	dispatch(getFreelancer(id))
+}
+}, [freelancerLoading])
+if(freelancerLoading){
+	return<h2>Loading...</h2>
+}
+console.log(freelancerData)
 	const routes = [
 		{ id: 1, resumeId: 1, element: <Resume1 freelancerHobbies={hobbiesData} {...freelancer} freelancerPosition={skillsData} experiences={experiences} /> },
 		{ id: 2, resumeId: 2, element: <Resume2 freelancerHobbies={hobbiesData} {...freelancer} freelancerPosition={skillsData} experiences={experiences} /> },

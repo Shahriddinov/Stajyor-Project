0ea2f7f6e2ec45e1815326b5c898efad626abd44
Temur_Zhakibaev <temurzhakibaev@gmail.com>
2023-05-26 23:27:56 -0700

    for pull

--- src/pages/Sign/RegisterFreelancer/CareerSlider/CareerSlider.jsx
+++ src/pages/Sign/RegisterFreelancer/CareerSlider/CareerSlider.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import "./style.scss";
-import MyCareerLogo from "../../../../assets/images/Resume/my-career.svg";
+import React from 'react';
+import MyCareerLogo from '../../../../assets/images/Resume/my-career.svg';
+import './style.scss';
 
 function CareerSlider({ dot, activeDote, classNameLine }) {
   const mediaWidth = window.innerWidth;
 
   return (
     <div>
-      <div className="mycareer">
-        <img className="mycareer__image" src={MyCareerLogo} alt="My Career" />
+      <div className='mycareer'>
+        <img className='mycareer__image' src={MyCareerLogo} alt='My Career' />
 
-        <div className="mycareer__text_box">
-          <span className="mycareer__text">{activeDote.label}</span>
+        <div className='mycareer__text_box'>
+          <span className='mycareer__text'>{activeDote.label}</span>
         </div>
 
         {activeDote.id !== dot.length && (
           <span className={classNameLine}></span>
         )}
 
-        <div className="mycareer__content">
+        <div className='mycareer__content'>
           {dot.map((el, i) => (
             <React.Fragment key={i}>
               <div
                 className={`mycareer__round ${
-                  activeDote.label === el.label ? "mycareer__round--active" : ""
+                  activeDote.label === el.label ? 'mycareer__round--active' : ''
                 }`}
                 style={{
                   top:
                     activeDote.id === el.id
                       ? mediaWidth > 1500
-                        ? "150px"
-                        : "120px"
+                        ? '150px'
+                        : '120px'
                       : activeDote.id > el.id
                       ? `${el.id === 1 ? el.id * 10 : el.id * 12}px`
                       : null,
